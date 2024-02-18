a960249d5df8eeca34dd3f53151c88dec6d5ae3b
Senuja Jayasekara <137871237+cyrex2002@users.noreply.github.com>
2024-02-18 14:44:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const interval = setInterval(() => {
+    document.querySelector('.txtlogo').onmouseover= event => {
+        event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+                if (index < 5) {
+                    return event.target.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+
+        if (iteration >= event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+
+        iteration += 3;
+    };
+}, 30);
